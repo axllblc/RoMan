b46e4b3a0d93f0746556ca880e5a34b4e577c83b
Pierre SAUSSEREAU <pierre.saussereau@etu.univ-tours.fr>
2022-12-08 19:13:13 +0100

    Ajout de la classe SingletonConnection pour la connection à la base avec le fichier connectionBDD.txt

---
+++ src/main/java/fr/roman/dao/SingletonConnection.java
@@ -0,0 +1,91 @@
+package fr.roman.dao;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+/**
+ * Classe implémentant la connexion avec la base de donnée.
+ * Permet de retourner un objet Connection pour les DAO de l'application.
+ * Suit le patron de conception "Singleton".
+ */
+public class SingletonConnection {
+
+  private static Connection co;
+  private final String url;
+  private final String login;
+  private final String password;
+
+  /**
+   * Constructeur privé de la classe SingletonConnection.
+   *
+   * @throws Exception Si la connection n'a pas eu lieu.
+   */
+  private SingletonConnection() throws Exception {
+    try {
+      HashMap<String, String> donneesConnection = getDonneesConnection();
+      this.url = donneesConnection.get("url");
+      this.login = donneesConnection.get("login");
+      this.password = donneesConnection.get("password");
+      co = DriverManager.getConnection(url,login,password);
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new IOException("Erreur dans la lecture du fichier de connection connectionBDD.txt");
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new SQLException("La base de données est inaccessible." +
+                             "Vérifiez les informations de connection.");
+    }
+
+  }
+
+  /**
+   * Cette méthode sert à récupérer les informations stockées dans le fichier de connection à la base.
+   *
+   * @return Un HashMap des informations de connection trouvé.
+   * @throws IOException Si le fichier n'est pas lisible
+   */
+  private HashMap<String, String> getDonneesConnection() throws IOException {
+    BufferedReader br = new BufferedReader(new FileReader(getFichierConnection()));
+    HashMap<String, String> donnees = new HashMap<String, String>();
+    int indiceLigne = 0;
+    String ligne = br.readLine();
+    while(ligne != null){
+      // On parcourt chaque ligne du fichier de connection.
+      String[] colonnesLigne = ligne.split("="); // On récupère les informations avec le séparateur '='
+      donnees.put(colonnesLigne[0], colonnesLigne[1]);
+      indiceLigne++;
+      ligne = br.readLine();
+    }
+    br.close();
+    return donnees;
+  }
+
+  /**
+   * Méthode servant à récupérer le fichier contenant les informations de connection.
+   *
+   * @return Un objet File contenant les informations du fichier.
+   */
+  private File getFichierConnection() {
+    return new File("src/main/ressources/connectionBDD.txt");
+  }
+
+  /**
+   * Méthode statique permettant d'obtenir l'unique instance de Connection.
+   *
+   * @return L'objet Connection.
+   */
+  public static Connection getInstance() throws Exception {
+    if(co == null) {
+      try {
+        new SingletonConnection();
+      } catch (Exception e) {
+        throw e;
+      }
+    }
+    return co;
+  }
+
+}
