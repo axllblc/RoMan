09e570914a41abb298525b38cc06dc07bdc8fa04
Pierre SAUSSEREAU <pierre.saussereau@etu.univ-tours.fr>
2022-12-14 17:58:41 +0100

    Peaufinage de la Javadoc de la classe DAO

--- src/main/java/fr/roman/dao/DAO.java
+++ src/main/java/fr/roman/dao/DAO.java
@@ -6,24 +6,31 @@ import java.util.HashMap;
 import java.util.stream.Collectors;
 
 /**
- * Classe abstraite DAO qui définie les principales méthodes des classes filles.
- * Possède un attribut Connection pour la liaison avec la base de donnée.
- * Est paramétré d'un modèle (M) de l'application et de l'énumération de ses attributs/champs (C).
+ * Classe abstraite {@link DAO} qui définie les principales méthodes des classes filles.
+ * Possède un attribut {@link Connection} pour la liaison avec la base de donnée.
+ * Est paramétré d'un ({@link M modèle M}) de l'application et de l'énumération de ses attributs/champs ({@link C C}).
  */
 public abstract class DAO<M, C extends Enum<C>> {
 
+  /**
+   * Objet {@link Connection} servant à établir la connection avec la base de donnée.
+   */
   private Connection co;
 
   /**
    * Constructeur.
    *
-   * @param co L'objet Connection obtenu à partir de la classe SingletonConnection.
+   * @param co L'objet {@link Connection} obtenu à partir de la classe {@link SingletonConnection}.
    *           Sert à effectuer la connection avec la base de données.
    */
   public DAO(Connection co) {
     this.co = co;
   }
 
+  /**
+   * Permet de retrouver l'objet {@link Connection} servant à JDBC
+   * @return
+   */
   public Connection getCo() {
     return co;
   }
@@ -31,15 +38,15 @@ public abstract class DAO<M, C extends Enum<C>> {
   /**
    * Entrée d'une ligne dans la table.
    *
-   * @param o Un objet métier.
-   * @return L'objet métier avec son identifiant ou null si l'insertion n'a pas eu lieu.
+   * @param o Un {@link M objet métier}.
+   * @return {@link M L'objet métier} avec son identifiant ou null si l'insertion n'a pas eu lieu.
    */
   public abstract M insert(M o);
 
   /**
    * Mise à jour d'une ligne dans une table de la base.
    *
-   * @param o Un objet métier.
+   * @param o Un {@link M objet métier.
    * @return True si la ligne a été modifiée, false sinon.
    */
   public abstract boolean update(M o);
@@ -55,17 +62,17 @@ public abstract class DAO<M, C extends Enum<C>> {
   /**
    * Recherche de données dans une table de la base avec les attributs renseignés de l'objet métier.
    *
-   * @param criteres Un objet HashMap<C, String> où la clé est le nom du critère
-   *                 (énumération des champs dans l'objet métier) et la valeur est celle du critère.
-   * @return Une collection d'objets qui correspond aux critères mis en paramètre.
+   * @param criteres Un objet HashMap<C, String> où ({@link C C} est la clé est le nom du critère
+   *                 (énumération des champs dans l'objet métier) et la {@link String valeur} est celle du critère.
+   * @return Une {@link Collection collection}d'{@link M objets métiers} qui correspond aux {@link C critères} mis en paramètre.
    */
   public abstract Collection<M> find(HashMap<C, String> criteres);
 
   /**
    * Recherche d'une ligne dans une table de la base à partir de sa clé primaire
    *
-   * @param id L'identifiant de l'objet métier.
-   * @return L'objet métier contenant les informations de la ligne.
+   * @param id L'identifiant de l'{@link M objet métier}.
+   * @return L'{@link M objet métier} contenant les informations de la ligne.
    *         Renvoie null si la ligne n'a pas été trouvée.
    */
   public abstract M findById(int id);
@@ -73,19 +80,27 @@ public abstract class DAO<M, C extends Enum<C>> {
   /**
    * Recherche de l'ensemble des données de la table.
    *
-   * @return Une collection d'objets de l'ensemble des données de la table.
+   * @return Une {@link Collection collection} d'objets de l'ensemble des données de la table.
    */
   public Collection<M> findAll(){
     // On réutilise la méthode find avec aucun critère comme paramètre
     return find(new HashMap<C, String>());
   }
 
+  /**
+   * Méthode permettant construire les critères de recherches (après la clause WHERE dans la requête SQL)
+   *  pour la méthode {@link #find}.
+   *
+   * @param criteres Un objet {@link HashMap}<{@link C}, {@link String}> où {@link C C}, la clé, est le nom du critère
+   *                 ({@link Enum<> énumération} des champs dans l'objet métier) et la {@link String valeur} est celle du critère.
+   * @return Une {@link String chaine de caractères} correspondant à ce qu'il faut mettre après la clause WHERE.
+   */
   String criteresPourWHERE(HashMap<C, String> criteres){
-
+    // Réalisé à l'aide d'expressions régulières et de l'API Stream.
     return criteres.entrySet()
             .stream()
-            .filter(c -> !c.getValue().isBlank())
+            .filter(c -> !c.getValue().isBlank()) // On retire les critères sans valeurs
             .map(c -> " AND " + c.getKey() + " LIKE '" + c.getValue() + "'")
-            .collect(Collectors.joining());
+            .collect(Collectors.joining()); // On concatène les conditions
   }
 }
