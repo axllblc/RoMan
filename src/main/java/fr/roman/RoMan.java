a3e4f40e8538435fbc9e431d4cccbe0681b4d602
mmaury2727 <mariussademaury4141@gmail.com>
2022-12-19 00:52:56 +0100

    Maquette V2

--- src/main/java/fr/roman/RoMan.java
+++ src/main/java/fr/roman/RoMan.java
@@ -2,17 +2,19 @@ package fr.roman;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import fr.roman.authentification.VueAuthentification;
 
 /**
  * Classe principale de l'application, utilisée comme point d'entrée pour l'exécution.
  */
 public class RoMan extends Application {
   public static void main(String[] args) {
-    launch(args);
+    Application.launch(args);
+
   }
 
   @Override
-  public void start(Stage primaryStage) {
-    // TODO à implémenter
+  public void start(Stage primaryStage) throws Exception {
+    VueAuthentification launcher = new VueAuthentification(primaryStage);
   }
 }
