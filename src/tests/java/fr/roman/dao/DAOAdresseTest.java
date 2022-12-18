2d0e0eb6d9c40c36e98270a63f97576b3e1e0eb6
Valentin Leclair <lym.valentin@gmail.com>
2022-12-18 14:09:42 +0100

    Initialisation de la branche testDAO avec ajout du répertoire tests et de la classe DAOAdresseTest. Ajout du fichier connectionBDD au gitignore.

---
+++ src/tests/java/fr/roman/dao/DAOAdresseTest.java
@@ -0,0 +1,26 @@
+package fr.roman.dao;
+
+import org.junit.jupiter.api.Test;
+
+class DAOAdresseTest {
+
+  @Test
+  void insert() {
+  }
+
+  @Test
+  void update() {
+  }
+
+  @Test
+  void delete() {
+  }
+
+  @Test
+  void find() {
+  }
+
+  @Test
+  void findById() {
+  }
+}
