dc7d86c1590cf3004c567b792a0553e5478b1d19
Valentin Leclair <lym.valentin@gmail.com>
2022-12-19 00:18:05 +0100

    Test des méthodes update et delete.

--- src/tests/java/fr/roman/dao/DAOAdresseTest.java
+++ src/tests/java/fr/roman/dao/DAOAdresseTest.java
@@ -7,8 +7,8 @@ class DAOAdresseTest {
   private final DAOAdresse TEST_DAO = new DAOAdresse();
 
   /**
-   * Compare le résultat de la méthode insert()
-   * seul l'idAdresse ne doit pas être égal.
+   * Compare le résultat de la méthode insert().
+   * Seul l'idAdresse ne doit pas être égal.
    */
   @Test
   void insert() {
@@ -43,12 +43,59 @@ class DAOAdresseTest {
     assertNotEquals(addresseAttendu.getIdAdresse(), retourAdresse.getIdAdresse());
   }
 
+  /**
+   * Test de la méthode update().
+   * Dépend des méthodes insert() et findById().
+   */
   @Test
   void update() {
+    // création d'un objet Adresse sans identifiant.
+    double[] coord = {40.0, 0.80};
+    Adresse testAddresse = new Adresse();
+    testAddresse.setCoordonneesGPS(coord);
+    testAddresse.setLibelle("test");
+    testAddresse.setNumeroVoie(1);
+    testAddresse.setComplementNumero("numéro");
+    testAddresse.setVoie("voie");
+    testAddresse.setComplementAdresse("adresse");
+    testAddresse.setCodePostal(37000);
+    testAddresse.setVille("tours");
+    Adresse retourAdresse = TEST_DAO.insert(testAddresse);
+
+    // modification de l'objet Adresse.
+    retourAdresse.setVille("paris");
+    assertTrue(TEST_DAO.update(retourAdresse));
+    // recherche l'objet modifier
+    Adresse updateAdresse = TEST_DAO.findById(retourAdresse.getIdAdresse());
+    // test si l'objet a bien été modifié.
+    assertEquals(retourAdresse,updateAdresse);
   }
 
+  /**
+   * Test la méthode delete() via l'idAresse.
+   * Dépend des méthodes insert() et findById().
+   */
   @Test
   void delete() {
+    // création d'un objet Adresse sans identifiant.
+    double[] coord = {40.0, 0.80};
+    Adresse testAddresse = new Adresse();
+    testAddresse.setCoordonneesGPS(coord);
+    testAddresse.setLibelle("test");
+    testAddresse.setNumeroVoie(1);
+    testAddresse.setComplementNumero("numéro");
+    testAddresse.setVoie("voie");
+    testAddresse.setComplementAdresse("adresse");
+    testAddresse.setCodePostal(37000);
+    testAddresse.setVille("tours");
+    Adresse retourAdresse = TEST_DAO.insert(testAddresse);
+
+    // test suppression de la dernière adresse ajouter à la BD.
+    assertTrue(TEST_DAO.delete(retourAdresse.getIdAdresse()));
+    // test recherche de l'adresse supprimer.
+    assertNull(TEST_DAO.findById(retourAdresse.getIdAdresse()));
+    // test suppression d'une adresse qui n'existe pas dans la BD.
+    assertFalse(TEST_DAO.delete(retourAdresse.getIdAdresse()+1));
   }
 
   @Test
