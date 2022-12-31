0297a24bdfc45c96ee7d8dcb636fa4a22b0c8b6d
Pierre SAUSSEREAU <pierre.saussereau@etu.univ-tours.fr>
2022-12-31 20:09:00 +0100

    ajout contrôleur (abstrait) pour les vues d'éditions

---
+++ src/main/java/fr/roman/controleurs/edition/CtrlEdition.java
@@ -0,0 +1,114 @@
+package fr.roman.controleurs.edition;
+
+import fr.roman.modeles.ChampsModele;
+import fr.roman.modeles.Modele;
+import fr.roman.modeles.Role;
+import fr.roman.vues.edition.VueEdition;
+import javafx.scene.Node;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Classe abstraite qui définit le contrôleur des vues d'édition (ajout ou modification de métiers)
+ * @param <M> Un métier qui étends {@link Modele}
+ * @param <C> Une énumération de ses champs, qui étends {@link ChampsModele}
+ */
+public abstract class CtrlEdition<M extends Modele, C extends Enum & ChampsModele> {
+
+    /**
+     * Le type de controleur : création ou modification
+     */
+    private final TypeEdition typeEdition;
+    /**
+     * Le rôle de l'utilisateur qui verra la vue (cf {@link Role})
+     */
+    private final Role role;
+    /**
+     * La vue (d'édition) que la classe contrôle
+     */
+    private final VueEdition vueEdition;
+    /**
+     * Le métier dans lequel des stockés les informations de la vue (vide si typeEdition = CREATION)
+     */
+    private M modele;
+    /**
+     * Carte des champs du formulaire de la vue (clé : nom d'un champ dans l'énum, valeur : un objet {@link Node}
+     */
+    Map<C, Node> champsFormulaire;
+
+    /**
+     * Constructeur du contrôleur
+     * @param m Le métier dans lequel des stockés les informations de la vue (vide si typeEdition = CREATION)
+     * @param vueEdition La vue (d'édition) que la classe contrôle
+     * @param typeEdition Le type de contrôleur : création ou modification
+     * @param role Le rôle de l'utilisateur qui verra la vue (cf {@link Role})
+     */
+    CtrlEdition(M m, VueEdition vueEdition, TypeEdition typeEdition, Role role) {
+        this.modele = m;
+        this.vueEdition = vueEdition;
+        this.typeEdition = typeEdition;
+        this.role = role;
+
+        vueEdition.setCtrl(this);
+
+        champsFormulaire = new LinkedHashMap<>();
+        /* L'implémentation LinkedHashMap a été choisie, car les nœuds sont dans l'ordre dans lequel
+         * les champs ont été insérées. */
+
+        chargerChamps();
+        accesChamps();
+    }
+
+    /**
+     * Classe à implémenter qui construit les objets {@link Node} à mettre dans la vue,
+     *  c.-à-d. les champs du formulaire (préremplis le cas échéant)
+     */
+    abstract void chargerChamps();
+
+    /**
+     * Classe permettant de restreindre l'accessibilité (la possibilité de modifier)
+     *  les champs du formulaire selon le {@link Role rôle} de l'utilisateur
+     */
+    void accesChamps(){
+        switch (getRole()) {
+            case ADMINISTRATEUR -> getChampsFormulaire().forEach((nom, noeud) -> noeud.setDisable(!nom.modifAdmin));
+            case PRODUCTEUR -> getChampsFormulaire().forEach((nom, noeud) -> noeud.setDisable(!nom.modifProd));
+            case ROOT -> getChampsFormulaire().forEach((nom, noeud) -> noeud.setDisable(nom.id));
+        }
+    }
+
+    /**
+     * Classe abstraite appelée par le bouton de validation du formulaire pour
+     *  effectuer l'ajout ou la modification dans la base de données des champs renseignés/modifiés.
+     * @return L'objet métier correspondant à ce qui a été ajouté dans la base de données
+     */
+    public abstract M validerSaisie();
+
+    /**
+     * Classe abstraite appelée par le bouton de retour du formulaire pour
+     *  annuler la saisie.
+     */
+    public void annulerSaisie() {
+    }
+
+    public TypeEdition getTypeEdition() {
+        return typeEdition;
+    }
+
+    public VueEdition getVueEdition() {
+        return vueEdition;
+    }
+
+    public M getModele() {
+        return modele;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public Map<C, Node> getChampsFormulaire() {
+        return champsFormulaire;
+    }
+}
