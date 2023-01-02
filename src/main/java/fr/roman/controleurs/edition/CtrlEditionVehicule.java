96a869e7e8f49f854d9cacde45404d658c578859
Pierre SAUSSEREAU <pierre.saussereau@etu.univ-tours.fr>
2023-01-02 17:18:58 +0100

    ajout (premier jet) de l'implémentation du controleur pour l'edition d'un véhicule

---
+++ src/main/java/fr/roman/controleurs/edition/CtrlEditionVehicule.java
@@ -0,0 +1,152 @@
+package fr.roman.controleurs.edition;
+
+import fr.roman.dao.DAOClient;
+import fr.roman.dao.DAOVehicule;
+import fr.roman.dao.DAOProducteur;
+import fr.roman.modeles.*;
+import fr.roman.vues.edition.VueEdition;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+
+import java.util.*;
+
+public class CtrlEditionVehicule extends CtrlEdition<Vehicule, Vehicule.Champs> {
+
+    // Les DAOs nécessaire pour le fonctionnement du contrôleur
+    DAOVehicule daoVehicule;
+    DAOProducteur daoProducteur;
+    DAOClient daoClient;
+
+    {
+        try {
+            daoVehicule = new DAOVehicule();
+            daoClient = new DAOClient();
+            daoProducteur = new DAOProducteur();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Constructeur du contrôleur
+     * @param Vehicule Le Vehicule dans laquelle des stockés les informations de la vue
+     *                 (si role = producteur, on inclut déjà l'instance du producteur,
+     *                 même pour typeEdition = CREATION)
+     * @param vueEdition La vue (d'édition) que la classe contrôle
+     * @param typeEdition Le type de contrôleur : création ou modification
+     * @param role Le rôle de l'utilisateur qui verra la vue (cf {@link Role})
+     */
+    public CtrlEditionVehicule(Vehicule Vehicule, VueEdition vueEdition, TypeEdition typeEdition, Role role) {
+        super(Vehicule, vueEdition, typeEdition, role);
+    }
+
+    /**
+     * Classe à implémenter qui construit les objets {@link Node} à mettre dans la vue,
+     *  c.-à-d. les champs du formulaire (préremplis le cas échéant)
+     */
+    @Override
+    public void chargerChamps() {
+        // idVehicule
+        TextField idVehicule = new TextField();
+        if(getTypeEdition() == TypeEdition.MODIFICATION){
+            idVehicule.setText(String.valueOf(getModele().getIdVehicule()));
+        }
+        getChampsFormulaire().put(Vehicule.Champs.idVehicule, idVehicule);
+
+        // immatriculation
+        TextField immatriculation = new TextField();
+        if(getTypeEdition() == TypeEdition.MODIFICATION){
+            immatriculation.setText(getModele().getImmatriculation());
+        }
+        immatriculation.setTextFormatter(new TextFormatter<>(change -> {
+            if (!change.getControlNewText().matches("[0-9A-Za-z]{0,7}")) {
+                return null;
+            } else {
+                change.setText(change.getText().toUpperCase());
+                return change;
+            }
+        }));
+        getChampsFormulaire().put(Vehicule.Champs.immatriculation, immatriculation);
+
+        // poidsMax
+        TextField poidsMax = new TextField();
+        if(getTypeEdition() == TypeEdition.MODIFICATION){
+            poidsMax.setText(String.valueOf(getModele().getPoidsMax()));
+        }
+        poidsMax.setTextFormatter(new TextFormatter<>(change -> {
+            if (!change.getControlNewText().matches("\\d{0,5}")) {
+                return null;
+            } else {
+                return change;
+            }
+        }));
+        getChampsFormulaire().put(Vehicule.Champs.poidsMax, poidsMax);
+
+        // libelle
+        TextField libelle = new TextField();
+        if(getTypeEdition() == TypeEdition.MODIFICATION){
+            libelle.setText(getModele().getLibelle());
+        }
+        libelle.setTextFormatter(new TextFormatter<>(change -> {
+            if (!change.getControlNewText().matches(".{0,50}")) {
+                return null;
+            } else {
+                return change;
+            }
+        }));
+        getChampsFormulaire().put(Vehicule.Champs.libelle, libelle);
+
+        // producteur
+        TextField idProducteur = new TextField();
+        idProducteur.setTextFormatter(new TextFormatter<>(change -> {
+            if (!change.getControlNewText().matches("\\d*")) {
+                return null;
+            } else {
+                return change;
+            }
+        }));
+        if(getTypeEdition() == TypeEdition.MODIFICATION || getRole() == Role.PRODUCTEUR){
+            idProducteur.setText(String.valueOf(getModele().getProducteur().getIdProducteur()));
+        }
+        getChampsFormulaire().put(Vehicule.Champs.idProducteur, idProducteur);
+
+        getVueEdition().definirChamps(getChampsFormulaire(), "Véhicule");
+    }
+
+    /**
+     * Classe appelée par le bouton de validation du formulaire pour
+     *  effectuer l'ajout ou la modification dans la base de données des champs renseignés/modifiés.
+     * @return L'objet métier correspondant à ce qui a été ajouté dans la base de données
+     */
+    @Override
+    public Vehicule validerSaisie() {
+        // TODO: 02/01/2023 vérifier que tous les champs obligatoires sont saisis
+
+        champsFormulaire = (Map<Vehicule.Champs, Node>) getVueEdition().getChamps();
+
+        // immatriculation
+        getModele().setImmatriculation(((TextField) champsFormulaire.get(Vehicule.Champs.immatriculation)).getText());
+
+        // poids
+        getModele().setPoidsMax(Integer.parseInt(((TextField) champsFormulaire.get(Vehicule.Champs.poidsMax)).getText()));
+
+        // libelle
+        getModele().setLibelle(((TextField) champsFormulaire.get(Vehicule.Champs.libelle)).getText());
+
+        // idProducteur
+        Producteur p = daoProducteur.findById(Integer.parseInt(((TextField) champsFormulaire
+                                                     .get(Vehicule.Champs.idProducteur)).getText()));
+        getModele().setProducteur(p);
+
+        Vehicule Vehicule = null;
+        switch (getTypeEdition()){
+            case CREATION -> Vehicule = daoVehicule.insert(getModele());
+            case MODIFICATION -> {  if(daoVehicule.update(getModele()))
+                                        Vehicule = getModele();
+                                 }
+
+        }
+        return Vehicule;
+    }
+}
--- src/main/java/fr/roman/modeles/Vehicule.java
+++ src/main/java/fr/roman/modeles/Vehicule.java
@@ -83,7 +83,71 @@ public class Vehicule extends Modele {
     this.producteur = producteur;
   }
 
-  public enum Champs {immatriculation, libelle, poidsMax, icProducteur, idVehicule}
+  public enum Champs implements ChampsModele {
+    idVehicule(true, false, false, false, false),
+    immatriculation(false, true, true, false, false),
+    poidsMax(false, true, true, false, false),
+    libelle,
+    idProducteur(false, false, true, true, false);
+    private final boolean modifProd;
+    private final boolean modifAdmin;
+    private final boolean id;
+    private final boolean idExt;
+    private final boolean nullable;
+    /**
+     * Constructeur pour désigner des propriétés du champ
+     *
+     * @param id True si il s'agit de l'identifiant, false sinon
+     * @param modifProd True si le champ est modifiable par un producteur, false sinon
+     * @param modifAdmin True si le champ est modifiable par un administrateur, false sinon
+     * @param idExt True s'il s'agit d'un identifiant qui correspond
+     *              à une clé étrangère dans la BDD, false sinon
+     * @param nullable True si le champ peut être null, false sinon
+     */
+    Champs(boolean id, boolean modifProd, boolean modifAdmin, boolean idExt, boolean nullable) {
+      this.id = id;
+      this.modifProd = modifProd;
+      this.modifAdmin = modifAdmin;
+      this.idExt = idExt;
+      this.nullable = nullable;
+    }
+    /**
+     * Par défaut, tous les utilisateurs peuvent modifier le champ,
+     * ce n'est pas un id et il est nullable
+     */
+    Champs(){
+      modifProd = true;
+      modifAdmin = true;
+      id = false;
+      idExt = false;
+      nullable = true;
+    }
+
+    @Override
+    public boolean isModifProd() {
+      return this.modifProd;
+    }
+
+    @Override
+    public boolean isModifAdmin() {
+      return this.modifAdmin;
+    }
+
+    @Override
+    public boolean isId() {
+      return this.id;
+    }
+
+    @Override
+    public boolean isIdExt() {
+      return this.idExt;
+    }
+
+    @Override
+    public boolean isNullable() {
+      return this.nullable;
+    }
+  }
 
   @Override
   public boolean equals(Object o) {
