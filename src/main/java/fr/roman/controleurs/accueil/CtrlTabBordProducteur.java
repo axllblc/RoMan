950e20d5d6d0b17f3ab856e5f95669fb1d763b4e
Axel Leblanc <axel.leblanc@etu.univ-tours.fr>
2023-01-05 12:39:20 +0100

    Tableau de bord : remplissage des tableaux

--- src/main/java/fr/roman/controleurs/accueil/CtrlTabBordProducteur.java
+++ src/main/java/fr/roman/controleurs/accueil/CtrlTabBordProducteur.java
@@ -1,8 +1,19 @@
 package fr.roman.controleurs.accueil;
 
+import fr.roman.RoManErreur;
+import fr.roman.dao.DAOCommande;
+import fr.roman.dao.DAOProducteur;
+import fr.roman.dao.DAOTournee;
+import fr.roman.modeles.Commande;
+import fr.roman.modeles.Producteur;
 import fr.roman.modeles.Role;
+import fr.roman.modeles.Tournee;
 import fr.roman.modeles.Utilisateur;
 import fr.roman.vues.accueil.TableauDeBordProducteur;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * Contrôleur de la vue <i>Tableau de bord</i> pour les producteurs.
@@ -10,8 +21,9 @@ import fr.roman.vues.accueil.TableauDeBordProducteur;
  * @see TableauDeBordProducteur
  */
 public class CtrlTabBordProducteur {
-  private Utilisateur utilisateur;
-  private TableauDeBordProducteur vue;
+  private Producteur producteur;
+  private final Utilisateur utilisateur;
+  private final TableauDeBordProducteur vue;
 
   /**
    * Initialiser le contrôleur de la vue <i>Tableau de bord</i> pour un utilisateur ayant le rôle
@@ -30,14 +42,57 @@ public class CtrlTabBordProducteur {
 
     vue.setCtrl(this);
 
-    // TODO à implémenter
-    // Instanciation du contrôleur du tableau pour les commandes
-    // Instanciation et appel du DAO Commandes
-    // Insertion des commandes non affectées à une tournée dans le tableau
+    try {
+      // Récupération du producteur lié à l'utilisateur actuel
+      DAOProducteur daoProducteur = new DAOProducteur();
+      producteur = daoProducteur.find(utilisateur);
 
-    // Instanciation du contrôleur du tableau pour les tournées
-    // Instanciation et appel du DAO Tournées
-    // Insertion des tournées à venir
+      if (producteur == null) {
+        throw new RuntimeException("Le compte utilisateur n'est pas lié à un producteur.");
+      }
+
+      // Remplissage des tableaux
+      tableauCommandes();
+      tableauTournees();
+    } catch (Exception e) {
+      RoManErreur.afficher(e);
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  /**
+   * Remplissage du tableau des commandes.
+   */
+  private void tableauCommandes() throws Exception {
+    // Récupération des commandes (non livrées)
+    DAOCommande daoCommande = new DAOCommande();
+    LinkedHashMap<Commande.Champs, String> criteres = new LinkedHashMap<>();
+    criteres.put(Commande.Champs.idProducteur, String.valueOf(producteur.getId()));
+    criteres.put(Commande.Champs.dateLivraison, null);
+    ArrayList<Commande> commandes = daoCommande.find(criteres);
+
+    // Ajout des commandes au tableau
+    vue.getTableauCommandes().setContenu(commandes);
+  }
+
+  /**
+   * Remplissage du tableau des tournées.
+   */
+  private void tableauTournees() throws Exception {
+    // Récupération des tournées
+    DAOTournee daoTournee = new DAOTournee();
+    LinkedHashMap<Tournee.Champs, String> criteres = new LinkedHashMap<>();
+    criteres.put(Tournee.Champs.idProducteur, String.valueOf(producteur.getId()));
+    List<Tournee> tournees = daoTournee.find(criteres);
+
+    // Ne conserver que les tournées à venir (le DAO ne permet pas ce filtrage)
+    tournees = tournees.stream()
+        .filter(tournee -> tournee.getHoraireDebut().after(Calendar.getInstance()))
+        .toList();
+
+    // Ajout des tournées au tableau
+    vue.getTableauTournees().setContenu(tournees);
   }
 
   public void nouvelleCommande() {
